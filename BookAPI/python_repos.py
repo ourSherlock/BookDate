1ab80a85c0a5eb624752e1ccfd982dec2d1efb80
工藤新一 <185960940@qq.com>
2021-10-21 16:24:35 +0800

    完成了第十七章(主要第对pygal和调用API的使用)

--- BookAPI/python_repos.py
+++ BookAPI/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -11,8 +13,45 @@ print("Total repositories:", response_dict['total_count'])
 
 # 探索有关仓库的信息
 repo_dicts = response_dict['items']
-print("Repsitories returned:", len(repo_dicts))
+print("Number of otems:", len(repo_dicts))
 
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description']
+    }
+
+# 可视化
+my_style=LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size =24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = "Most-Starred Python Projects on GitHub"
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
+
+"""print("\nSelected information about each repositorty")
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository::', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])"""
+
+'''
 # 研究第一个仓库
 repo_dict = repo_dicts[0]
 
@@ -23,4 +62,4 @@ print('Stars:', repo_dict['stargazers_count'])
 print('Repository:', repo_dict['html_url'])
 print('Created:', repo_dict['created_at'])
 print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+print('Description:', repo_dict['description'])'''
